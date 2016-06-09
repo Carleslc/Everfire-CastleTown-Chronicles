a99ad587fea139d1c6cb36e3ef326b0cc232f10e
wextia <valcazar96@gmail.com>
2016-06-09 14:41:56 +0200

    Created Map, activated text asset serialization.

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class that holds the information of various <c>Tiles</c>.
+/// </summary>
+public class Map {
+    Tile[,] map;
+    int height, width;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="height">Height of the map, in tiles.</param>
+    /// <param name="width">Width of the map, in tiles.</param>
+    public Map(int height, int width) {
+        map = new Tile[height, width];
+        this.height = height;
+        this.width = width;
+    }
+
+    /// <summary>
+    /// Returns the type of the tile in the <c>Pos</c> p. Prints an error and returns an empty <c>Tile</c>if
+    /// p is an invalid <c>Pos</c>.
+    /// </summary>
+    /// <param name="p">Position occupied by the tile to be retrieved.</param>
+    /// <returns>Returns the <c>Tile</c> in p. Returns an empty <c>Tile</c>if
+    /// p is an invalid <c>Pos</c>.
+    /// </returns>
+    public Tile GetTileType(Pos p) {
+        Pos p1 = new Pos(1, 2);
+        p1 = p1.Left();
+        if (isOutOfBounds(p)) {
+            Debug.LogError("Tile is out of bounds.");
+            return new Tile();
+        }
+        return map[p.X, p.Y];
+    }
+
+    /// <summary>
+    /// Returns if the <c>Tile</c> in p is walkable. Prints an error and returns false if
+    /// p is an invalid <c>Pos</c>.
+    /// </summary>
+    /// <param name="p">Position occupied by the tile to be retrieved.</param>
+    /// <returns>Returns if the <c>Tile</c> in p is walkable. Returns false if
+    /// p is an invalid <c>Pos</c>.
+    /// </returns>
+    public bool IsWalkable(Pos p) {
+        if (isOutOfBounds(p)) {
+            Debug.LogError("Tile is out of bounds.");
+            return false;
+        }
+        return map[p.X, p.Y].isWalkable();
+    }
+
+    /// <summary>
+    /// Returns whether if p is out of bounds in the <c>Map</c> or not.
+    /// </summary>
+    /// <param name="p">The position to be investigated.</param>
+    /// <returns>true if p is out of bounds in the <c>Map</c>, false if it is not.</returns>
+    public bool isOutOfBounds(Pos p) {
+        return p.X >= width || p.X < 0 || p.Y >= height || p.Y < 0;
+    }
+
+    /// <summary>
+    /// Sets the <c>Tile</c> in p to the <c>Tile</c> t. Prints an error if
+    /// p is an invalid <c>Pos</c>.
+    /// </summary>
+    /// <param name="p">The position occupied by the tile to be modified.</param>
+    /// <param name="t">The new <c>Tile</c>Tile to occupy p with.</param>
+    public void SetTile(Pos p, Tile t) {
+        if (isOutOfBounds(p))
+        {
+            Debug.LogError("Tile is out of bounds.");
+            return;
+        }
+        map[p.X, p.Y] = t;
+    }
+}
+
+/// <summary>
+/// Class that stores a position inside the <b>Map</b>. The X value represents the <b>horizontal</b> axis of the map,
+/// and the Y value the <b>vertical</b> axis.
+/// </summary>
+public class Pos{
+    int x;
+    int y;
+
+    /// <summary>
+    /// The position's <b>horizontal</b> axis' value.
+    /// </summary>
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+    }
+
+    /// <summary>
+    /// The position's <b>vertical</b> axis' value.
+    /// </summary>
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+    }
+    /// <summary>
+    /// Default Constructor.
+    /// </summary>
+    /// <param name="x">The position's <b>horizontal</b> axis' value.</param> 
+    /// <param name="y">The position's <b>vertical</b> axis' value.</param>
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /// <summary>
+    /// Returns a new Pos.
+    /// </summary>
+    /// <returns>The position directly to the <b>left</b> of the instance this function is being called on.</returns>
+    public Pos Left() {
+        return new Pos(x - 1, y);
+    }
+
+    /// <summary>
+    /// Returns a new Pos.
+    /// </summary>
+    /// <returns>The position directly to the <b>right</b> of the instance this function is being called on.</returns>
+    public Pos Right()
+    {
+        return new Pos(x + 1, y);
+    }
+
+    /// <summary>
+    /// Returns a new Pos.
+    /// </summary>
+    /// <returns>The position directly <b>above</b> of the instance this function is being called on.</returns>
+    public Pos Up()
+    {
+        
+        return new Pos(x, y + 1);
+    }
+
+    /// <summary>
+    /// Returns a new Pos.
+    /// </summary>
+    /// <returns>The position directly <b>below</b> of the instance this function is being called on.</returns>
+    public Pos Down() {
+        return new Pos(x, y - 1);
+    }
+}
+
+public enum Tile
+{
+    grass, tree
+}
+public static class TileExtensions {
+    public static bool isWalkable(this Tile tile) {
+        return true;
+    }
+}
