60acb02a1368717f95de92030d6fd980897f5d74
Carleslc <lazaro.costa.carles@gmail.com>
2016-06-10 05:54:07 +0200

    Tiles now have two levels and different types
    Map needs to be re-adapted to new tile types.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,45 +1,123 @@
 ﻿using System;
 using System.Reflection;
+using TileExtensions;
 
-public enum Tile
+public class Tile
 {
-    [TileAttr(true)] Grass,
+    Ground ground;
+    Object above;
+
     /// <summary>
-    /// The base of a Tree.
+    /// The base ground (lowest level) of this tile.
     /// </summary>
-    [TileAttr(false)] Tree,
-    [TileAttr(false)] Stone,
-    [TileAttr(false)] Water
-}
-
-class TileAttr : Attribute
-{
-    public bool Walkable { get; private set; }
-
-    internal TileAttr(bool walkable)
-    {
-        Walkable = walkable;
+    public Ground GroundType {
+        get { return ground; }
     }
-}
 
-static class TileExtensions
-{
     /// <summary>
-    /// Checks if the tile is walkable or not.
+    /// The object above the ground (highest level) of this tile.
+    /// </summary>
+    public Object ObjectAbove {
+        get { return above; }
+        set { above = value; }
+    }
+
+    /// <summary>
+    /// Constructs new tile with a base ground and without any object stacked above.
+    /// </summary>
+    /// <param name="groundType">Base ground type of the tile</param>
+    public Tile(Ground groundType)
+    {
+        ground = groundType;
+        above = Object.Empty;
+    }
+
+    /// <summary>
+    /// Checks if there is an object above the ground of this tile.
+    /// </summary>
+    /// <returns><c>true</c> if this tile has a non-empty object, <c>false</c> otherwise.</returns>
+    public bool HasObjectAbove()
+    {
+        return above != Object.Empty;
+    }
+
+    /// <summary>
+    /// Checks whether this tile is walkable or not.
+    /// <para/>A tile is walkable when both ground and object on top are walkable.
     /// </summary>
     /// <returns><c>true</c> if this tile is walkable, <c>false</c> otherwise.</returns>
-    public static bool isWalkable(this Tile tile)
+    public bool IsWalkable()
     {
-        return GetAttr(tile).Walkable;
+        return ground.IsWalkable() && above.IsWalkable();
     }
 
-    private static TileAttr GetAttr(Tile p)
+    /// <summary>
+    /// Tile type that represents the base ground of a tile
+    /// </summary>
+    public enum Ground
     {
-        return (TileAttr)Attribute.GetCustomAttribute(ForValue(p), typeof(TileAttr));
+        [TileAttr(true)]
+        Grass,
+        [TileAttr(true)]
+        Sand,
+        [TileAttr(false)]
+        Water
     }
 
-    private static MemberInfo ForValue(Tile p)
+    /// <summary>
+    /// Tile type that can be stacked above the ground
+    /// </summary>
+    public enum Object
     {
-        return typeof(Tile).GetField(Enum.GetName(typeof(Tile), p));
+        [TileAttr(true)]
+        Empty,
+        /// <summary>
+        /// The base of a Tree.
+        /// </summary>
+        [TileAttr(false)]
+        Tree,
+        [TileAttr(false)]
+        Stone
+    }
+
+}
+
+/// <summary>
+/// This namespace only must be used in this class for Tile enums additional information.
+/// This namespace has been created for restrict access to attributes outside Tile class.
+/// </summary>
+namespace TileExtensions
+{
+    internal static class TileExtensions {
+
+        /// <summary>
+        /// Checks whether this tile type is walkable or not.
+        /// </summary>
+        /// <returns><c>true</c> if this tile is walkable, <c>false</c> otherwise.</returns>
+        internal static bool IsWalkable(this Enum e)
+        {
+            return GetAttr(e).Walkable;
+        }
+
+        private static TileAttr GetAttr(Enum e)
+        {
+            return (TileAttr)Attribute.GetCustomAttribute(ForValue(e), typeof(TileAttr));
+        }
+
+        private static MemberInfo ForValue(Enum e)
+        {
+            return typeof(Enum).GetField(Enum.GetName(typeof(Enum), e));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    internal class TileAttr : Attribute
+    {
+        public bool Walkable { get; private set; }
+
+        public TileAttr(bool walkable)
+        {
+            Walkable = walkable;
+        }
     }
 }
