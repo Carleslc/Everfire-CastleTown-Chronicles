8a621ef9aeeafd15fd105f734466429155d15f48
wextia <valcazar96@gmail.com>
2016-07-05 16:38:38 +0200

    Clases de Edificios de Explotación DONE

--- Assets/Scripts/Building/StorageBuilding.cs
+++ Assets/Scripts/Building/StorageBuilding.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public abstract class StorageBuilding : Building {
+public abstract class StorageBuilding : Building
+{
 #pragma warning disable 0414
     int maxCapacity;
     int usedCapacity;
@@ -36,7 +37,7 @@ public abstract class StorageBuilding : Building {
         set
         {
             usedCapacity = value;
-            if(usedCapacity >= maxCapacity)
+            if (usedCapacity >= maxCapacity)
             {
                 isFull = true;
                 usedCapacity = maxCapacity;
@@ -44,7 +45,7 @@ public abstract class StorageBuilding : Building {
         }
     }
 
-    public StorageBuilding(int maxCapacity, BuildingType buildingType, int width, int depth, Pos location, Village village, int hitPoints) : 
+    public StorageBuilding(int maxCapacity, BuildingType buildingType, int width, int depth, Pos location, Village village, int hitPoints) :
         base(buildingType, width, depth, location, village, hitPoints)
     {
         if (buildingType == BuildingType.warehouse)
@@ -56,6 +57,37 @@ public abstract class StorageBuilding : Building {
     }
 
     /// <summary>
+    /// Deletes an <c>ammount</c> ammount of the resource <c>resource</c>. Returns the ammount that remains of the given
+    /// resourceType.
+    /// </summary>
+    /// <param name="resource"></param>
+    /// <param name="ammount"></param>
+    /// <returns></returns>
+    public int Retrieve(ResourceType resource, int ammount)
+    {
+        if (storedGoods.ContainsKey(resource))
+        {
+            storedGoods[resource] -= ammount;
+            if (storedGoods[resource] < 0)
+            {
+                int ret = storedGoods[resource];
+                storedGoods[resource] = 0;
+                return ret;
+            }
+        }
+
+        //If the storedGood does not exist, we return the ammount to be stored, which means that nothing was retrieved.
+        return ammount;
+    }
+
+    public int GetStoredAmount(ResourceType resource)
+    {
+        int ret = 0;
+        storedGoods.TryGetValue(resource, out ret);
+        return ret;
+    }
+
+    /// <summary>
     /// Stores an ammount of a given resource in this building. Returns 0 if it is successful. If it returns more that that it
     /// means that the returned ammount of resources could not be stored in there.
     /// </summary>
@@ -64,7 +96,8 @@ public abstract class StorageBuilding : Building {
     /// <returns></returns>
     public int Store(ResourceType resource, int ammount)
     {
-        if (!allowedGoods.Contains(resource)) {
+        if (!allowedGoods.Contains(resource))
+        {
             throw new System.Exception("Building " + BuildingType.ToString() + " does not allow this kind of resource: " +
                 resource.ToString());
         }
@@ -80,7 +113,7 @@ public abstract class StorageBuilding : Building {
         int notStoredAmmount = 0;
         if (usedCapacity + ammount > maxCapacity)
         {
-            notStoredAmmount =  maxCapacity - usedCapacity + ammount;
+            notStoredAmmount = maxCapacity - usedCapacity + ammount;
         }
         //We dont care if it overflows, the property takes care of it.
         UsedCapacity += ammount;
--- Assets/Scripts/Building/Workplace.cs
+++ Assets/Scripts/Building/Workplace.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class Workplace : StorageBuilding
 {
-#pragma warning disable 0414
     ResourceType rawMaterial;
-#pragma warning disable 0414
     ResourceType processedGood;
     int productionGoal;
     int productionProcess = 0;
@@ -15,10 +13,22 @@ public class Workplace : StorageBuilding
     {
         rawMaterial = job.RawMaterial();
         processedGood = job.ProcessedGood();
-        //We allow to have food and stuff in here, because every Workplace is a home
-        allowedGoods.Add(ResourceType.bread);
-        allowedGoods.Add(ResourceType.sausage);
+        InitAllowedGoods();
+        productionGoal = processedGood.ProductionTime();
     }
+
+    private void InitAllowedGoods()
+    {
+        allowedGoods.AddRange(new ResourceType[]
+        {
+            //We allow to have food and stuff in here, because every Workplace is a home
+            ResourceType.bread,
+            ResourceType.sausage,
+            rawMaterial,
+            processedGood
+        });
+    }
+
     /// <summary>
     /// Returns the ammount of productionTime remaining unitill the resource is processed.
     /// </summary>
@@ -26,16 +36,28 @@ public class Workplace : StorageBuilding
     /// <returns></returns>
     public int Process(int speed)
     {
-        //If the raw material IS NOT a map resource
-        if(rawMaterial.ProductionTime() > 0)
+        int timeLeft = productionProcess - productionGoal;
+        if (GetStoredAmount(rawMaterial) > 0)
         {
-            int ammount;
-            //We have to chech if the rawMaterial is stored in the building
-            if (!storedGoods.TryGetValue(rawMaterial, out ammount) || ammount <= 0)
-                throw new System.Exception(rawMaterial.ToString() + " not stored in " + BuildingType.ToString());
-            //We do this because with map resources we can't have them stored in our building, we have to collect them.
+            //If the raw material IS NOT a map resource
+            if (rawMaterial.ProductionTime() > 0)
+            {
+                int ammount;
+                //We have to chech if the rawMaterial is stored in the building
+                if (!storedGoods.TryGetValue(rawMaterial, out ammount) || ammount <= 0)
+                    throw new System.Exception(rawMaterial.ToString() + " not stored in " + BuildingType.ToString());
+                //We do this because with map resources we can't have them stored in our building, we have to collect them.
+            }
+            productionProcess += speed;
+            timeLeft = productionProcess - productionGoal;
+            if (timeLeft <= 0)
+            {
+                if (Retrieve(rawMaterial, 1) == 1)
+                    throw new System.Exception("Production cannot be finished in Workplace " + BuildingType.ToString() +
+                        " because there is not enough raw material stored.");
+                Store(processedGood, 1);
+            }
         }
-        productionProcess += speed;
-        return productionProcess - productionGoal;
+        return timeLeft > 0 ? timeLeft : 0;
     }
 }
