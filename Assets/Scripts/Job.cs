08fbba2375acf334ab681b58f534a0ac1dde450d
wextia <valcazar96@gmail.com>
2016-07-05 11:00:46 +0200

    Typos

--- Assets/Scripts/Job.cs
+++ Assets/Scripts/Job.cs
@@ -14,7 +14,7 @@ public enum Job
     [JobAttr(ResourceType.deer, ResourceType.meat)]
     hunter,
     [JobAttr(ResourceType.stone, ResourceType.stoneBlock)]
-    stonecutter
+    stonecutter,
     //this enum type is used in WorkerManagerEditor.cs
     dirty,
     //this one is used in BuildingType
